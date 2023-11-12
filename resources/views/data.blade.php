97765c301d806e1a87345787260e270209e81462
Ed-Sokolov <ed.sokolov33@gmail.com>
2023-11-12 13:10:35 +0200

    Changed ticks in graphs and tab-content style

--- resources/views/data.blade.php
+++ resources/views/data.blade.php
@@ -5,7 +5,9 @@
         <div class="tab-wrapper d-flex align-items-start" data-init="tab">
             <div class="nav flex-column nav-pills me-3 gap-2" id="v-pills-tab" role="tablist" aria-orientation="vertical"></div>
 
-            <div class="tab-content w-100" id="v-pills-tabContent"></div>
+            <div class="card w-100 p-3">
+                <div class="tab-content" id="v-pills-tabContent"></div>
+            </div>
         </div>
     </div>
 @endsection
